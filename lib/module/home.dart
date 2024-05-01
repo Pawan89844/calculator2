6e9abf3fff6eacb3680ca9c1d3d115f01bca109d
Pawan Kumar <pk89844@gmail.com>
2024-05-01 17:02:21 +0530

    Grid Built

--- lib/module/home.dart
+++ lib/module/home.dart
@@ -1,29 +1,77 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
-  Container _onFirstRow(i) {
-    List<IconData> operator = [
-      Icons.clear,
-      Icons.mode,
-      Icons.remove,
-      Icons.dangerous
-    ];
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int j = -1;
+  Container _onFirstRow(int i) {
+    List<String> operator = ['AC', '%', '()'];
+    return Container(
+        alignment: Alignment.center,
+        child: TextButton(onPressed: () {}, child: Text(operator[i])));
+  }
+
+  Container _onLastColumn(int i) {
+    List<String> operators = ['÷', 'x', '-', '+', '='];
+    return Container(
+        alignment: Alignment.center,
+        child:
+            TextButton(onPressed: () {}, child: Text(operators[i].toString())));
+  }
+
+  String _numbers(int i) {
+    List<int> numbers = [];
+    numbers.add(i);
+    numbers = numbers.reversed.toList();
+    return '${i - 4}';
+  }
+
+  Container _digits(int i) {
     return Container(
         // color: Colors.blue,
         alignment: Alignment.center,
-        child: IconButton(onPressed: () {}, icon: Icon(operator[i])));
+        child: TextButton(onPressed: () {}, child: Text(_numbers(i))));
+  }
+
+  @kDebugMode
+  void _incrJ() {
+    if (j < 4) {
+      j++;
+    } else {
+      j = -1;
+      j++;
+    }
+  }
+
+  Container _onLastRow(int i) {
+    if (i == 17) {
+      return Container(
+          alignment: Alignment.center,
+          child: TextButton(onPressed: () {}, child: const Text('•')));
+    } else {
+      return Container(
+        alignment: Alignment.center,
+        child: TextButton(onPressed: () {}, child: const Text('⌫')),
+      );
+    }
   }
 
   Container _gridBody(int i) {
-    if (i >= 4) {
-      return Container(
-          // color: Colors.blue,
-          alignment: Alignment.center,
-          child: TextButton(onPressed: () {}, child: Text('${i - 4}')));
-    } else if (i == 0 || i <= 3) {
+    if (i == 3 || i == 7 || i == 11 || i == 15 || i == 19) {
+      _incrJ();
+      return _onLastColumn(j);
+    } else if (i == 0 || i < 3) {
       return _onFirstRow(i);
+    } else if (i == 17 || i == 18) {
+      return _onLastRow(i);
+    } else if (i >= 4 || i <= 9) {
+      return _digits(i);
     } else {
       return Container();
     }
@@ -36,6 +84,7 @@ class Home extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
+          const Divider(color: Colors.black12),
           GridView(
             physics: const NeverScrollableScrollPhysics(),
             shrinkWrap: true,
